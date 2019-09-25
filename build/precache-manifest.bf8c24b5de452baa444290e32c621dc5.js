d38e5d45c17c6c7449d83632590f53c065251635
Vincent Kuertz <vincent.kuertz@gmail.com>
2019-09-24 21:02:16 -0400

    removed build from .gitignore

---
+++ build/precache-manifest.bf8c24b5de452baa444290e32c621dc5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf7f815e69cedb81fa0f7e03ad24ceb9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93e993505659a6292ac9",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "42a09813dc1c2c64fcfc",
+    "url": "/static/js/2.bcff73b8.chunk.js"
+  },
+  {
+    "revision": "93e993505659a6292ac9",
+    "url": "/static/js/main.25bb9ba9.chunk.js"
+  },
+  {
+    "revision": "e80861b612dd47ab2bdc",
+    "url": "/static/js/runtime~main.98ffff15.js"
+  }
+]);
